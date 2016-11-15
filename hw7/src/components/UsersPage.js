37cb49c8a3ea606492c13326100088f5c4956d66
liu115 <jimmydiablo@gmail.com>
2016-11-15 22:06:02 +0800

    build UsersPage.js

--- hw7/src/components/UsersPage.js
+++ hw7/src/components/UsersPage.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 
 
 class UsersPage extends Component {
+  state = {
+    users: []
+  };
+
   componentDidMount() {
     // fetch `/api/users` to get users and then set state...
+    fetch('/api/users')
+    .then(response => response.json())
+    .then(json => {
+      this.setState({ users: json.users });
+    }).catch(error => {
+      console.log('request failed', error);
+    });
   }
 
   render() {
     return (
       <div>Users
-        <li><a href="#/users/1">User 1</a></li>
+        <ul>
+          {this.state.users.map((val, i) =>
+            <li key={i}><a href={`#/users/${i}`}>User {i}</a></li>
+          )}
+        </ul>
       </div>
     );
   }
