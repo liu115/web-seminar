80476b4afed67f6ba7f8b96f5b1c72e0ab734c78
liu115 <jimmydiablo@gmail.com>
2016-11-15 22:36:42 +0800

    build SingleUserPage component

--- hw7/src/components/SingleUserPage.js
+++ hw7/src/components/SingleUserPage.js
@@ -6,12 +6,36 @@ class SingleUserPage extends Component {
     id: PropTypes.string.isRequired,
   };
 
+  state = {
+    name: '',
+    avatar: '',
+    age: 0,
+  };
+
   componentDidMount() {
     // fetch `/api/users/${id}` to get user and then set state...
+    fetch(`/api/users/${this.props.id}`)
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        name: json.name,
+        avatar: json.avatar,
+        age: json.age,
+      });
+    }).catch(error => {
+      console.log('request failed', error);
+    });
   }
 
   render() {
-    return <div>User {this.props.id}</div>;
+    return (
+      <div>
+        User {this.props.id}
+        <p>Name: {this.state.name}</p>
+        <p>Age: {this.state.age}</p>
+        <img src={this.state.avatar} alt="avatar" />
+      </div>
+    );
   }
 }
 
