8615596a61fda79a5ee8f33b4532a325c9d07baf
liu115 <jimmydiablo@gmail.com>
2016-11-15 21:23:36 +0800

    build /users and /users/:id api

--- hw7/api.js
+++ hw7/api.js
@@ -2,11 +2,23 @@ import { Router } from 'express';
 
 const router = new Router();
 
+const db = {
+  users: [
+    { avator: 'http://lorempixel.com/200/200/people', name: 'John', age: 23 },
+    { avator: 'http://lorempixel.com/200/200/people', name: 'Amy', age: 18 },
+  ]
+};
 
 // Write your restful api here:
-router.get('/', (req, res) => {
-
+router.get('/users', (req, res) => {
+  res.send(JSON.stringify(db));
 });
-
+router.get('/users/:id', (req, res) => {
+  if (req.params.id < db.users.length) {
+    res.send(JSON.stringify(db.users[req.params.id]));
+  } else {
+    res.send(JSON.stringify({}));
+  }
+});
 
 export default router;
